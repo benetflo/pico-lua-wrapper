ccbd023a7137831f86e45cc0052eb0e456a55d2e
Benji <ben.flodin@outlook.com>
2026-02-11 22:47:13 +0100

    Delete lua_script.h

--- lua_script.h
+++
@@ -1,8 +0,0 @@
-const char *lua_script = 
-"while true do\n"
-"    hardware.gpio.set_output(16, 1)\n"
-"    hardware.timer.delay_ms(300)\n"
-"    hardware.gpio.set_output(16, 0)\n"
-"    hardware.timer.delay_ms(300)\n"
-"end\n"
-;
